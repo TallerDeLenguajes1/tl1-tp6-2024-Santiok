ec81434cf4fd814d93d4525b0deeb683d3ad50d0
“Santiago” <santimedinaok.10@gmail.com>
2024-05-14 21:58:05 -0300

    Ejercicio 2 completo

--- Program.cs
+++ Program.cs
@@ -31,3 +31,70 @@ Console.WriteLine("\nEl numero invertido es: " + invertido);
 
 //EJERCICIO 2.
 
+int opcion, num1, num2, resultado;
+
+Console.WriteLine("\nIngrese una opcion:");
+Console.WriteLine("1)Sumar");
+Console.WriteLine("2)Restar");
+Console.WriteLine("3)Multiplicar");
+Console.WriteLine("4)Dividir");
+int.TryParse(Console.ReadLine(), out opcion);
+
+switch (opcion)
+{
+    case 1:
+
+        Console.WriteLine("\nIngrese el primer numero");
+        int.TryParse(Console.ReadLine(), out num1);
+        Console.WriteLine("\nIngrese el segundo numero");
+        int.TryParse(Console.ReadLine(), out num2);
+
+        resultado = num1 + num2;
+
+        Console.WriteLine("El resultado es: " + resultado);
+
+    break;
+
+    case 2:  
+
+        Console.WriteLine("\nIngrese el primer numero");
+        int.TryParse(Console.ReadLine(), out num1);
+        Console.WriteLine("\nIngrese el segundo numero");
+        int.TryParse(Console.ReadLine(), out num2);
+
+        resultado = num1 - num2;
+
+        Console.WriteLine("El resultado es: " + resultado);
+
+    break;
+
+    case 3:  
+
+        Console.WriteLine("\nIngrese el primer numero");
+        int.TryParse(Console.ReadLine(), out num1);
+        Console.WriteLine("\nIngrese el segundo numero");
+        int.TryParse(Console.ReadLine(), out num2);
+
+        resultado = num1 * num2;
+
+        Console.WriteLine("El resultado es: " + resultado);
+
+    break;
+
+    case 4: 
+     
+        Console.WriteLine("\nIngrese el primer numero");
+        int.TryParse(Console.ReadLine(), out num1);
+        Console.WriteLine("\nIngrese el segundo numero");
+        int.TryParse(Console.ReadLine(), out num2);
+
+        resultado = num1 / num2;
+
+        Console.WriteLine("El resultado es: " + resultado);
+
+    break;
+
+    default:
+    Console.WriteLine("La opcion ingresada es incorrecta.");
+    break;
+}
